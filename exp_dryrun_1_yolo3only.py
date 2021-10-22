8a88781d1802fd2bf3dc21aa54b5546b4eb21a1e
Baris Cekic <baris@diskover.net>
2021-10-22 02:09:43 +0100

    merge

--- exp_dryrun_1_yolo3only.py
+++ exp_dryrun_1_yolo3only.py
@@ -11,4 +11,6 @@ exp.run_experiment()
 exp.evaluate_results()
 exp.plot_results() # some models inference are higher than 5sec in CPU.
 
+
+# hacky solution to seperate yolov3 and yolov5, they dont run mutually. 
 os.system("mv 'experiments/" + str(exp.experiment_name) + "/stats/stats.json' 'experiments/" + str(exp.experiment_name) + "/stats/stats_yolov3.json' ")
